4874851b7ecc712ae99316d4d7412bd381d7fe8d
pnTrungPham <trung.nhatrangart@gmail.com>
2020-06-03 16:56:08 +0700

    Update prefix for class in file autoload

--- includes/File_manager/lib/php/autoload.php
+++ includes/File_manager/lib/php/autoload.php
@@ -1,8 +1,8 @@
 <?php
 
-define('ELFINDER_PHP_ROOT_PATH', dirname(__FILE__));
+define('NJT_ELFINDER_PHP_ROOT_PATH', dirname(__FILE__));
 
-function elFinderAutoloader($name)
+function njt_elFinderAutoloader($name)
 {
     $map = array(
         'elFinder' => 'elFinder.class.php',
@@ -29,18 +29,18 @@ function elFinderAutoloader($name)
         'elFinderVolumeTrash' => 'elFinderVolumeTrash.class.php',
     );
     if (isset($map[$name])) {
-        return include_once(ELFINDER_PHP_ROOT_PATH . '/' . $map[$name]);
+        return include_once(NJT_ELFINDER_PHP_ROOT_PATH . '/' . $map[$name]);
     }
     $prefix = substr($name, 0, 14);
     if (substr($prefix, 0, 8) === 'elFinder') {
         if ($prefix === 'elFinderVolume') {
-            $file = ELFINDER_PHP_ROOT_PATH . '/' . $name . '.class.php';
+            $file = NJT_ELFINDER_PHP_ROOT_PATH . '/' . $name . '.class.php';
             return (is_file($file) && include_once($file));
         } else if ($prefix === 'elFinderPlugin') {
-            $file = ELFINDER_PHP_ROOT_PATH . '/plugins/' . substr($name, 14) . '/plugin.php';
+            $file = NJT_ELFINDER_PHP_ROOT_PATH . '/plugins/' . substr($name, 14) . '/plugin.php';
             return (is_file($file) && include_once($file));
         } else if ($prefix === 'elFinderEditor') {
-            $file = ELFINDER_PHP_ROOT_PATH . '/editors/' . substr($name, 14) . '/editor.php';
+            $file = NJT_ELFINDER_PHP_ROOT_PATH . '/editors/' . substr($name, 14) . '/editor.php';
             return (is_file($file) && include_once($file));
         }
     }
@@ -48,8 +48,8 @@ function elFinderAutoloader($name)
 }
 
 if (version_compare(PHP_VERSION, '5.3', '<')) {
-    spl_autoload_register('elFinderAutoloader');
+    spl_autoload_register('njt_elFinderAutoloader');
 } else {
-    spl_autoload_register('elFinderAutoloader', true, true);
+    spl_autoload_register('njt_elFinderAutoloader', true, true);
 }
 
