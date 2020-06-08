b24284a4707f3e863482d29ad6fa50246e33560f
pnTrungPham <trung.nhatrangart@gmail.com>
2020-06-08 10:19:17 +0700

    Fix css text heading

--- views/pages/html-filemanager.php
+++ views/pages/html-filemanager.php
@@ -4,15 +4,15 @@ $lang = !empty( $this->options['njt_fs_file_manager_settings']['fm_locale']) ? $
 ?>
 <div class="njt-fs-file-manager">
   <div class="njt-fs-select-theme">
-    <div class="wrap njt-fs-mr0">
+    <div class="njt-fs-wrap njt-fs-mr0">
       <h1 class="wp-heading-inline njt-fs-pd0"><?php _e("Filester - WordPress File Manager Pro", NJT_FS_BN_DOMAIN); ?></h1>
     </div>
     <div class="select-theme-content">
       <?php    
       $selectedTheme = get_option('njt_fs_selector_themes') && get_option('njt_fs_selector_themes')[$this->userRole]['themesValue'] ? get_option('njt_fs_selector_themes')[$this->userRole]['themesValue'] : null;
     ?>
-      <div class="wrap njt-fs-mr0">
-        <h3 class="select-theme-title"><?php _e("Select theme:", NJT_FS_BN_DOMAIN); ?></h3>
+      <div class="njt-fs-wrap njt-fs-mr0">
+        <h3 class="wp-heading-inline select-theme-title"><?php _e("Select theme:", NJT_FS_BN_DOMAIN); ?></h3>
       </div>
       <select name="selector-themes" id="selector-themes">
         <option value="Default"><?php _e("Default Elfinder", NJT_FS_BN_DOMAIN); ?></option>
