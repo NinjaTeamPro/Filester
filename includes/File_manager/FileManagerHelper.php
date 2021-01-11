35520adfbefeaf885d9c0481f8b1312fdd195453
pnTrungPham <trung.nhatrangart@gmail.com>
2021-01-11 09:32:04 +0700

    Add mime type .xlsm

--- includes/File_manager/FileManagerHelper.php
+++ includes/File_manager/FileManagerHelper.php
@@ -91,6 +91,7 @@ class FileManagerHelper{
 			'.pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
 			'.xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
 			'.xlsb' => 'application/vnd.ms-excel.sheet.binary.macroenabled.12',
+			'.xlsm' => 'application/vnd.ms-excel.sheet.macroenabled.12',
 			'.dotx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
 			'.xltx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
 			'.potx' => 'application/vnd.openxmlformats-officedocument.presentationml.template',
