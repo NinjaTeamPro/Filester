6701c20cbbc407cc2264780afae01d6195d0c1d6
pnTrungPham <65383916+pnTrungPham@users.noreply.github.com>
2023-11-08 10:25:17 +0700

    Update code

--- includes/File_manager/lib/php/autoload.php
+++ includes/File_manager/lib/php/autoload.php
@@ -1,8 +1,8 @@
 <?php
 
-define('NJT_FS_ELFINDER_PHP_ROOT_PATH', dirname(__FILE__));
+define('ELFINDER_PHP_ROOT_PATH', dirname(__FILE__));
 
-function njt_fs_elFinderAutoloader($name)
+function elFinderAutoloader($name)
 {
     $map = array(
         'elFinder' => 'elFinder.class.php',
@@ -30,18 +30,18 @@ function njt_fs_elFinderAutoloader($name)
         'elFinderVolumeTrash' => 'elFinderVolumeTrash.class.php',
     );
     if (isset($map[$name])) {
-        return include_once(NJT_FS_ELFINDER_PHP_ROOT_PATH . '/' . $map[$name]);
+        return include_once(ELFINDER_PHP_ROOT_PATH . '/' . $map[$name]);
     }
     $prefix = substr($name, 0, 14);
     if (substr($prefix, 0, 8) === 'elFinder') {
         if ($prefix === 'elFinderVolume') {
-            $file = NJT_FS_ELFINDER_PHP_ROOT_PATH . '/' . $name . '.class.php';
+            $file = ELFINDER_PHP_ROOT_PATH . '/' . $name . '.class.php';
             return (is_file($file) && include_once($file));
         } else if ($prefix === 'elFinderPlugin') {
-            $file = NJT_FS_ELFINDER_PHP_ROOT_PATH . '/plugins/' . substr($name, 14) . '/plugin.php';
+            $file = ELFINDER_PHP_ROOT_PATH . '/plugins/' . substr($name, 14) . '/plugin.php';
             return (is_file($file) && include_once($file));
         } else if ($prefix === 'elFinderEditor') {
-            $file = NJT_FS_ELFINDER_PHP_ROOT_PATH . '/editors/' . substr($name, 14) . '/editor.php';
+            $file = ELFINDER_PHP_ROOT_PATH . '/editors/' . substr($name, 14) . '/editor.php';
             return (is_file($file) && include_once($file));
         }
     }
@@ -49,8 +49,8 @@ function njt_fs_elFinderAutoloader($name)
 }
 
 if (version_compare(PHP_VERSION, '5.3', '<')) {
-    spl_autoload_register('njt_fs_elFinderAutoloader');
+    spl_autoload_register('elFinderAutoloader');
 } else {
-    spl_autoload_register('njt_fs_elFinderAutoloader', true, true);
+    spl_autoload_register('elFinderAutoloader', true, true);
 }
 
